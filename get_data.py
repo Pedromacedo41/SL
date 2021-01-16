024fe861011fbbead6f2b046d5b66c01332ec908
Matheus Centa <matheuscenta@gmail.com>
2021-01-16 18:38:23 +0100

    Update get_data.py script to add housing dataset

--- get_data.py
+++ get_data.py
@@ -15,8 +15,7 @@ def get_auto_mpg():
     url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data'
 
     df = pd.read_csv(url, sep='\t', header=None)
-    df['all'] = df[0]
-    df = df.drop(columns=[0, 1]) # drop car names
+    df = df.drop(columns=[1]) # drop car names
 
     X, y = np.zeros((398, 7)), np.zeros(398)
     for i, row in df.iterrows():
@@ -29,7 +28,22 @@ def get_auto_mpg():
     save_to_csv('auto_mpg', X, y)
 
 
+def get_housing():
+    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data'
+
+    df = pd.read_csv(url, header=None)
+
+    X, y = np.zeros((506, 13)), np.zeros(506)
+    for i, row in df.iterrows():
+        split = np.array([float(num) for num in row[0].split()])
+        y[i] = split[-1]
+        X[i] = split[:-1]
+
+    save_to_csv('housing', X, y)
+
+
 if __name__ == "__main__":
     os.makedirs('data/', exist_ok=True)
 
     get_auto_mpg()
+    get_housing()
