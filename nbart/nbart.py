5bbc0512f440304cbf8653df9514fb9e659a627e
Matheus Centa <matheuscenta@gmail.com>
2021-01-19 00:21:08 +0100

    Add NBART training script

---
+++ nbart/nbart.py
@@ -0,0 +1,144 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class TreeNode(object):
+    """Represents a node from a BART tree."""
+    def __init__(
+        self, n_eta=None, split_attribute_M= 0, split_value=0, y_pred=None,
+        y_avg=None, posterior_var=None, posterior_mean=None, left=None,
+        right=None
+    ):
+        self.n_eta = n_eta
+        self.left = left
+        self.right = right
+
+        # attributes for internal nodes
+        self.split_attribute_M = split_attribute_M
+        self.split_value = split_value
+
+        # attributes for leaves
+        self.y_pred = y_pred
+        self.y_avg = y_avg
+        self.posterior_var = posterior_var
+        self.posterior_mean = posterior_mean
+
+
+class NBART(nn.Module):
+    """Neural Network derived from a single BART tree."""
+    def __init__(self, input_dim, inner_decisions, paths, gamma1=1.0, gamma2=1.0):
+        super(NBART, self).__init__()
+        self.gamma1 = gamma1
+        self.gamma2 = gamma2
+
+        self.dic = {}
+        self.lin1 = nn.Linear(input_dim, len(inner_decisions))
+        self.set_connections_1_layer(inner_decisions)
+
+        self.lin2 = nn.Linear(len(inner_decisions), 1 + len(inner_decisions))
+        self.avgs = []
+        self.set_connections_2_layer(paths)
+
+        self.lin3 = nn.Linear(1 + len(inner_decisions), 1)
+        self.set_connections_3_layer()
+
+    def forward(self, x):
+        out = self.lin1(x)
+        out = self.lin2(torch.tanh(self.gamma1 * out))
+        return self.lin3(torch.tanh(self.gamma2 * out))
+
+    def set_connections_1_layer(self, inner_decisions):
+        with torch.no_grad():
+            self.lin1.weight.data = torch.zeros(self.lin1.weight.shape)
+            self.lin1.bias.data = torch.zeros(self.lin1.bias.shape)
+            for i, elem in enumerate(inner_decisions):
+                # node k to layer index
+                self.dic[elem[0]] = i
+
+                self.lin1.weight.data[i, int(elem[1])] = 1
+                self.lin1.bias[i] = elem[2]
+
+    def set_connections_2_layer(self, paths):
+        with torch.no_grad():
+            self.lin2.weight.data = torch.zeros(self.lin2.weight.shape)
+            self.lin2.bias.data = torch.zeros(self.lin2.bias.shape)
+            for i, elem in enumerate(paths):
+                _, avg, tuples = elem
+                self.avgs.append(avg)
+                self.lin2.bias[i] = (-len(tuples) + 1/2)
+                for a in tuples:
+                    self.lin2.weight.data[i, self.dic[a[0]]] = a[1]
+
+    def set_connections_3_layer(self):
+        with torch.no_grad():
+            self.lin3.weight.data = (0.5 * torch.as_tensor(self.avgs)).reshape(1, -1)
+            self.lin3.bias.data = self.lin3.weight.data.sum()
+
+
+def inner_decisions(root, leafinfo, k):
+    decisions_pairs= []
+    all_paths = []
+    leafinfos = leafinfo.copy()
+
+    if root.left is None and root.right is None:
+        return decisions_pairs, [(k, root.posterior_mean, leafinfos)], k
+
+    decisions_pairs.append((k, root.split_attribute_M, root.split_value))
+
+    leafinfos.append((k, -1))
+    dec, paths, K =  inner_decisions(root.left, leafinfos, k + 1)
+    decisions_pairs.extend(dec)
+    all_paths.extend(paths)
+
+    leafinfos.pop()
+    leafinfos.append((k, +1))
+    dec, paths , K =  inner_decisions(root.right, leafinfos, K+1)
+    decisions_pairs.extend(dec)
+    all_paths.extend(paths)
+
+    return decisions_pairs, all_paths, K
+
+
+def load_trees(filename):
+    trees = []
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            vals = iter(line.split())
+
+            def helper():
+                val = next(vals)
+                if val == '#':
+                    return None
+
+                elems = val.split("@")
+                node = TreeNode(n_eta = float(elems[0]), split_attribute_M = float(elems[1]),
+                            split_value = float(elems[2]), y_pred=float(elems[3]),
+                             y_avg= float(elems[4]),posterior_var= float(elems[5]), posterior_mean=float(elems[6]))
+
+                node.left = helper()
+                node.right = helper()
+                return node
+
+            trees.append(helper())
+    return trees
+
+
+def to_nbart(trees, input_dim):
+    nns = []
+    for tree in trees:
+        decisions, paths, _ =  inner_decisions(tree, [], 0)
+        nns.append(NBART(input_dim, decisions, paths))
+    return nns
+
+
+if __name__ == "__main__":
+    left = TreeNode(n_eta=1, left = TreeNode(n_eta=2) , right =TreeNode(n_eta=3, left= TreeNode(n_eta=4), right= TreeNode(n_eta=5)))
+    right = TreeNode(n_eta=6, left = TreeNode(n_eta=7) , right =TreeNode(n_eta=8, left= TreeNode(n_eta=9), right= TreeNode(n_eta=10)))
+    tree = TreeNode(n_eta=0, left=left, right=right)
+    print(tree)
+
+    # test
+    decisions, paths, K = inner_decisions(tree, [], k = 0)
+    print(decisions)
+    print(paths)
---
+++ nbart/train.py
@@ -0,0 +1,145 @@
+from collections import defaultdict
+from math import sqrt
+import os
+import shutil
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import tqdm
+
+from nbart import load_trees, to_nbart
+
+
+def load_data(name):
+    # Load CSV to tensor
+    X = pd.read_csv('data/X_{}.csv'.format(name), header=None)
+    X = torch.Tensor(X.to_numpy())
+    y = pd.read_csv('data/y_{}.csv'.format(name), header=None)
+    y = torch.Tensor(y.to_numpy())
+
+    # 50/25/25 split into train/validation/test
+    n = len(X)
+    train_end, val_end = int(0.5 * n), int(0.75 * n)
+
+    X_train = X[:train_end]
+    X_val = X[train_end:val_end]
+    X_test = X[val_end:]
+
+    y_train = y[:train_end]
+    y_val = y[train_end:val_end]
+    y_test = y[val_end:]
+
+    # Create and return datasets
+    train = TensorDataset(X_train, y_train)
+    val = TensorDataset(X_val, y_val)
+    test = TensorDataset(X_test, y_test)
+    return train, val, test
+
+
+def predict(nbart, x):
+    out = nbart[0](x)
+    for m in nbart[1:]:
+        out += m(x)
+    return out
+
+
+def compute_rmse(nbart, loader):
+    with torch.no_grad():
+        rmse = None
+        for x, y in loader:
+            y_hat = predict(nbart, x)
+            rmse = torch.sqrt(torch.mean((y_hat - y) ** 2)).item()
+            break
+        return rmse
+
+
+def log(metrics, scalar_dict, step):
+    for k, v in scalar_dict.items():
+        metrics[k].append((step, v))
+    return metrics
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('name', type=str)
+    parser.add_argument('run', type=str)
+    parser.add_argument('--epochs', '-e', type=int, default=100)
+    parser.add_argument('--learning-rate', '-lr', type=float, default=1e-3)
+    parser.add_argument('--batch_size', '-b', type=int, default=32)
+    args = parser.parse_args()
+
+    # Prepare directories
+    model_name = "nbart_{}{}".format(args.name, args.run)
+    os.makedirs('logs/', exist_ok=True)
+    os.makedirs('models/', exist_ok=True)
+
+    if os.path.isdir('logs/{}/'.format(model_name)):
+        shutil.rmtree('logs/{}/'.format(model_name)) # remove previous logs
+
+    # Load data
+    train, val, test = load_data(args.name)
+    train_loader = DataLoader(train, batch_size=args.batch_size, shuffle=False)
+    val_loader = DataLoader(val, batch_size=len(val), shuffle=False)
+    test_loader = DataLoader(test, batch_size=len(test), shuffle=False)
+
+    # Set up logs
+    metrics = defaultdict(list)
+    writer = SummaryWriter('logs/{}'.format(model_name))
+
+    # Get model, optimizer and loss function
+    input_dim = train[0][0].shape[0]
+    trees = load_trees('models/bart_{}{}.model'.format(args.name, args.run))
+    nbart = to_nbart(trees, input_dim)
+
+    params = []
+    for m in nbart:
+        params.extend(list(m.parameters()))
+    optimizer = torch.optim.Adam(params, lr=args.learning_rate)
+    optimizer.zero_grad()
+
+    loss_fn = nn.MSELoss()
+
+    val_rmse = compute_rmse(nbart, val_loader)
+    test_rmse = compute_rmse(nbart, test_loader)
+    print("start:\n"
+          "  val rmse: {}\n"
+          "  test rmse: {}\n".format(
+            val_rmse, test_rmse))
+
+    for epoch in tqdm.trange(1, args.epochs + 1):
+        train_losses = []
+        for x, y in train_loader:
+            y_hat = predict(nbart, x)
+            loss = loss_fn(y_hat, y)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            train_losses.append(loss.detach().item())
+
+        train_rmse = sqrt(np.mean(np.array(train_losses)))
+        val_rmse = compute_rmse(nbart, val_loader)
+        test_rmse = compute_rmse(nbart, test_loader)
+
+        writer.add_scalars('rmse', {
+            'train': train_rmse,
+            'val': val_rmse,
+            'test': test_rmse,
+        }, epoch)
+        metrics = log(metrics, {
+            'train_rmse': train_rmse,
+            'val_rmse': val_rmse,
+            'test_rmse': test_rmse,
+        }, epoch)
+        if epoch % 5 == 0:
+            print("epoch {}\n"
+                  "  train rmse: {}\n"
+                  "  val rmse: {}\n"
+                  "  test rmse: {}\n".format(
+                      epoch, train_rmse, val_rmse, test_rmse))
+
+    writer.close()
